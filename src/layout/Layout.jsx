17f9060fdfa9c1fe9468df36fac70a7aa9301983
johurul-abir <mdjohurulislam555@gmail.com>
2024-08-05 00:52:18 +0600

    footer update

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Topbar from "../components/top bar/Topbar";
+import Footer from "../components/footer/Footer";
 
 const Layout = () => {
   return (
     <>
       <Topbar />
       <Outlet />
+      <Footer />
     </>
   );
 };
